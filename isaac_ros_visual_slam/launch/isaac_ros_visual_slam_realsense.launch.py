58fd48c4cae1ec7c6a592d0f0581a513d3d3b150
Andrew Brahim <dirksavage88@gmail.com>
2025-11-22 23:46:12 +0000

    launch file for generic stereo cam
    Signed-off-by: Andrew Brahim <dirksavage88@gmail.com>

--- isaac_ros_visual_slam/launch/isaac_ros_visual_slam_realsense.launch.py
+++ isaac_ros_visual_slam/launch/isaac_ros_visual_slam_realsense.launch.py
@@ -22,56 +22,39 @@ from launch_ros.descriptions import ComposableNode
 
 def generate_launch_description():
     """Launch file which brings up visual slam node configured for RealSense."""
-    realsense_camera_node = Node(
-        name='camera',
-        namespace='camera',
-        package='realsense2_camera',
-        executable='realsense2_camera_node',
-        parameters=[{
-            'enable_infra1': True,
-            'enable_infra2': True,
-            'enable_color': False,
-            'enable_depth': False,
-            'depth_module.emitter_enabled': 0,
-            'depth_module.profile': '640x360x90',
-            'enable_gyro': True,
-            'enable_accel': True,
-            'gyro_fps': 200,
-            'accel_fps': 200,
-            'unite_imu_method': 2
-        }],
-    )
 
     visual_slam_node = ComposableNode(
         name='visual_slam_node',
         package='isaac_ros_visual_slam',
         plugin='nvidia::isaac_ros::visual_slam::VisualSlamNode',
         parameters=[{
-            'enable_image_denoising': False,
-            'rectified_images': True,
+            'enable_image_denoising': True,
+            'rectified_images': False,
             'enable_imu_fusion': True,
-            'gyro_noise_density': 0.000244,
+            'gyro_noise_density': 0.001,
             'gyro_random_walk': 0.000019393,
             'accel_noise_density': 0.001862,
             'accel_random_walk': 0.003,
-            'calibration_frequency': 200.0,
-            'image_jitter_threshold_ms': 22.00,
-            'base_frame': 'camera_link',
-            'imu_frame': 'camera_gyro_optical_frame',
-            'enable_slam_visualization': True,
+            'calibration_frequency': 95.0,
+            'image_jitter_threshold_ms': 36.50,
+            'imu_jitter_threshold_ms': 10.0,
+            'base_frame': 'base_frame',
+            'imu_frame': 'imu',
+            'enable_slam_visualization': False,
+            'enable_localization_n_mapping': False,
             'enable_landmarks_view': True,
             'enable_observations_view': True,
-            'camera_optical_frames': [
-                'camera_infra1_optical_frame',
-                'camera_infra2_optical_frame',
-            ],
+            #'camera_optical_frames': [
+            #    'camera_optical_frame0',
+            #    'camera_optical_frame1',
+            #],
         }],
         remappings=[
-            ('visual_slam/image_0', 'camera/infra1/image_rect_raw'),
-            ('visual_slam/camera_info_0', 'camera/infra1/camera_info'),
-            ('visual_slam/image_1', 'camera/infra2/image_rect_raw'),
-            ('visual_slam/camera_info_1', 'camera/infra2/camera_info'),
-            ('visual_slam/imu', 'camera/imu'),
+            ('visual_slam/image_0', 'left/image_raw'),
+            ('visual_slam/camera_info_0', 'left/camera_info'),
+            ('visual_slam/image_1', 'right/image_raw'),
+            ('visual_slam/camera_info_1', 'right/camera_info'),
+            ('visual_slam/imu', 'vio_transform/imu'),
         ],
     )
 
@@ -84,4 +67,4 @@ def generate_launch_description():
         output='screen',
     )
 
-    return launch.LaunchDescription([visual_slam_launch_container, realsense_camera_node])
+    return launch.LaunchDescription([visual_slam_launch_container])
